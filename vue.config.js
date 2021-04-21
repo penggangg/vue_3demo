6117df6d5c0e1ba76f8b2e59e8115d5bf42dddb5
penggang <918800642@qq.com>
2021-04-21 17:35:21 +0800

    添加按需加vant加载配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const tsImportPluginFactory = require('ts-import-plugin'); // ts vant 配置按需加载
 module.exports = {
     devServer: {
         proxy: {
@@ -7,10 +9,32 @@ module.exports = {
             }
         }
     },
-    configureWebpack: (config) => {
+    configureWebpack: config => {
+        config.module.rules.push({
+            test: /\.(jsx|tsx|js|ts)$/,
+            loader: 'ts-loader',
+            options: {
+                transpileOnly: true,
+                getCustomTransformers: () => ({
+                    before: [tsImportPluginFactory({
+                        libraryName: 'vant',
+                        libraryDirectory: 'es',
+                        style: true
+                    })]
+                }),
+                compilerOptions: {
+                    module: 'es2015'
+                }
+            },
+            exclude: /node_modules/
+        });
         config.externals = {
             ZCTEncrypt: 'window.ZCTEncrypt'
         };
+        if (process.env.npm_config_report) {
+            config.plugins.push(new BundleAnalyzerPlugin());
+        }
     }
+    // 链式操作
 
 };
